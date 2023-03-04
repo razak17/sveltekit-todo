f9e9e860ffd6851d8b1d6bd91ee841b7b9a5bd28
Razak <moabdulrazak17@gmail.com>
2023-03-04 16:12:47 +0000

    chore: first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte todo app" />
+</svelte:head>
+
+<section>
+	<h1>Hello Mom!</h1>
+</section>
+
+<style>
+</style>
