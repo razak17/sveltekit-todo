94ba2991e432dace0b9ae556c1fbae06c2dfee17
Razak <moabdulrazak17@gmail.com>
2023-03-04 18:03:34 +0000

    feat(components): style todo form
    add svg sprites

--- src/lib/components/todo/TodoForm.svelte
+++ src/lib/components/todo/TodoForm.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<form>
+<form class="todo-form">
 	<div class="form-wrapper">
 		<input type="text" name="name" />
 
@@ -18,3 +18,38 @@
 		</svg>
 	</div>
 </form>
+
+<style>
+	.todo-form .form-wrapper {
+		position: relative;
+	}
+
+	.todo-form input,
+	.todo-form button {
+		position: relative;
+		width: 100%;
+		z-index: 1;
+		padding: 15px;
+	}
+
+	.todo-form svg {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+
+	.todo-form button {
+		color: var(--white);
+		text-transform: uppercase;
+	}
+
+	@media screen and (min-width: 600px) {
+		.todo-form {
+			display: grid;
+			grid-template-columns: 2fr 1fr;
+			grid-column-gap: 5px;
+		}
+	}
+</style>
