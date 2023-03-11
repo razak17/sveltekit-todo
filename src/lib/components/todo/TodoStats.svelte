738f11d8f42a93f403c28224a038c2a74ddb6c00
Razak <moabdulrazak17@gmail.com>
2023-03-11 15:12:05 +0000

    feat(components): add task stats

--- src/lib/components/todo/TodoStats.svelte
+++ src/lib/components/todo/TodoStats.svelte
@@ -1,15 +1,20 @@
-<script>
+<script lang="ts">
+	import { tasks } from '../../store/task';
+
+	$: totalCount = $tasks.length;
+	$: completeCount = $tasks.filter((task) => task.isComplete).length;
+	$: progressCount = $tasks.filter((task) => !task.isComplete).length;
 </script>
 
 <div class="todo-stats">
 	<div class="total-tasks">
-		Total Tasks: <span>0</span>
+		Total Tasks: <span>{totalCount}</span>
 	</div>
 	<div class="completed-tasks">
-		Completed Tasks: <span>0</span>
+		Completed Tasks: <span>{completeCount}</span>
 	</div>
 	<div class="remaining-tasks">
-		Remaining Tasks: <span>0</span>
+		Remaining Tasks: <span>{progressCount}</span>
 	</div>
 	<svg>
 		<use xlink:href="#stats" />
